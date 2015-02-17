1a439109e3b17a5a7cf28471d4b84dfc26439673
Valik Grinevich <valik.v1per@gmail.com>
2015-02-17 21:30:45 +0200

    make some updates

---
+++ src/UserBundle/Providers/VkontakteProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace UserBundle\Providers;
+
+use HWI\Bundle\OAuthBundle\OAuth\Response\UserResponseInterface;
+use UserBundle\Document\User;
+
+class VkontakteProvider
+{
+    public function setUserData(User $user, UserResponseInterface $response)
+    {
+
+    }
+}
