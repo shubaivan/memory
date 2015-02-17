d7396c5f8febfd1db28b93ca5e87ab551702a7bc
Valik Grinevich <valik.v1per@gmail.com>
2015-02-17 23:29:53 +0200

    make some updates

--- src/UserBundle/Providers/VkontakteProvider.php
+++ src/UserBundle/Providers/VkontakteProvider.php
@@ -9,5 +9,10 @@ class VkontakteProvider
 {
     public function setUserData(User $user, UserResponseInterface $response)
     {
+        $responseArray = $response->getResponse();
+
+        $user->setFirstName($responseArray['response'][0]['first_name']);
+        $user->setSecondName($responseArray['response'][0]['last_name']);
+//        $user->setEmail('id'.$user->getVkontakteId().'@example.com');
     }
 }
