d0240b4f0063cb7a3e82984e62ce91787734d66c
Valik Grinevich <valik.v1per@gmail.com>
2015-02-17 12:57:12 +0200

    update video controller

--- src/AppBundle/Controller/AlbumController.php
+++ src/AppBundle/Controller/AlbumController.php
@@ -1,8 +1,10 @@
 <?php
 namespace AppBundle\Controller;
 
+use AppBundle\Document\Album;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template as Template;
 
 class AlbumController extends Controller
 {
@@ -10,18 +12,30 @@ class AlbumController extends Controller
      * Render front page
      *
      * @return Response
+     *
+     * @Template()
      */
-    public function indexAction()
+    public function getAllAlbumAction()
     {
         $albums = $this->get('doctrine_mongodb.odm.document_manager')
             ->getRepository('AppBundle:Album')
             ->findAll();
 
-        return $this->render(
-            'AppBundle:Album:showAlbum.html.twig',
-            [
-                'albums' => $albums
-            ]
-        );
+        return [
+            'albums' => $albums
+        ];
+    }
+
+    /**
+     * @param Album $album
+     * @return array
+     *
+     * @Template()
+     */
+    public function getSingleAlbumAction(Album $album)
+    {
+        return [
+            "album" => $album
+        ];
     }
 }
