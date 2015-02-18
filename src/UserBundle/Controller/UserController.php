4166c745ac57275fd38e72c96d6233b6c2fee85f
Valik Grinevich <valik.v1per@gmail.com>
2015-02-18 23:54:42 +0200

    add Connect and User controller and update routing file

---
+++ src/UserBundle/Controller/UserController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use UserBundle\Form\Type\UpdateProfileType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template as Template;
+
+class UserController extends Controller
+{
+    /**
+     * @param Request $request
+     * @return array|\Symfony\Component\HttpFoundation\RedirectResponse
+     *
+     * @Template()
+     */
+    public function updateContactsAction(Request $request)
+    {
+        $userAuth = $this->getUser();
+
+        if (!$userAuth) {
+            return $this->redirect($this->generateUrl('app_get_all_news'));
+        }
+
+        $dm = $this->get('doctrine_mongodb.odm.document_manager');
+        $user = $dm->getRepository('UserBundle:User')->findOneById($userAuth->getId());
+
+        if (strstr($user->getEmail(),'@example.com')) {
+            $user->setEmail('');
+        }
+
+        $form = $this->CreateForm(new UpdateProfileType(), $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $dm->flush();
+
+            $router = $this->generateUrl("app_get_all_news");
+
+            return $this->redirect(
+                $router
+            );
+        }
+
+        return array(
+            'form' => $form->createView(),
+        );
+    }
+}
