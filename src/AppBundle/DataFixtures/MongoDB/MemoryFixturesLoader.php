ae484d37300144e953b8209fd6d72def12a4dc47
Valik Grinevich <valik.v1per@gmail.com>
2015-02-20 23:59:33 +0200

    add fixtures

--- src/AppBundle/DataFixtures/MongoDB/MemoryFixturesLoader.php
+++ src/AppBundle/DataFixtures/MongoDB/MemoryFixturesLoader.php
@@ -2,7 +2,14 @@
 
 namespace AppBundle\DataFixtures\MongoDB;
 
-class DataFixturesLoader
-{
+use Hautelook\AliceBundle\Alice\DataFixtureLoader;
 
+class MemoryFixturesLoader extends DataFixtureLoader
+{
+    public function getFixtures()
+    {
+        return [
+            __DIR__ . "/fixtures.yml"
+        ];
+    }
 }
