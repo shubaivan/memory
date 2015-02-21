075ea9b2445ea30778e2b3152d4efafa53be6823
Valik Grinevich <valik.v1per@gmail.com>
2015-02-21 23:09:38 +0200

    update News type

--- src/AppBundle/Form/Type/NewsType.php
+++ src/AppBundle/Form/Type/NewsType.php
@@ -6,7 +6,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class AlbumType extends AbstractType
+class NewsType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
